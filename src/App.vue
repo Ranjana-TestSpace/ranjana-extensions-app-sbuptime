2281ddc0f1f8d3177c72cc80e934f36f37cfb9f9
Ranjana NS <ranjanans@Ranjanas-MacBook-Pro.local>
2025-09-02 12:31:39 +0530

    Actual embed of SB uptime

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <div id="app">
-    <h1>Welcome to My Frontend!</h1>
+  <div class="iframe-wrapper">
+    <iframe
+      src="https://uptime.storyblok.com/"
+      class="uptime-iframe"
+      title="Storyblok Uptime"
+    ></iframe>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+.iframe-wrapper {
+  width: 100%;
+  height: 100vh; /* Full viewport height */
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.uptime-iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
 }
 </style>
